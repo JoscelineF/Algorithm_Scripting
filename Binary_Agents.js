080559de8934ce4c1c48228dce2b8f511e48015f
Josceline Feng <22095436+JoscelineF@users.noreply.github.com>
2017-12-04 23:02:48 +0800

    Add files via upload

---
+++ Binary_Agents.js
@@ -0,0 +1,13 @@
+function binaryAgent(str) {
+  var biCode = str.split(" ");
+  var result="";
+  var i=0;
+  var len=biCode.length;
+  while (i<len){
+    result+=String.fromCharCode(parseInt(biCode[i],2));
+    i++;
+  }
+  return result;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
